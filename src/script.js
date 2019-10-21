4b9d99ed6d5bf85bd209ab93bbe82f96db088906
Jose Luis Aguilar <jlaguigom@gmail.com>
2019-10-21 13:26:53 +0200

    reloj terminado

--- src/script.js
+++ src/script.js
@@ -19,15 +19,34 @@ let clock = {
 		return this.currentTime().getHours();
 	},
 	secondDegrees(){
-		return this.currentSecond()*360/60;
+		return this.currentSecond()*360/60-90;
 	},
 	minuteDegrees(){
-		return this.currentMinute()*360/60;
+		return this.currentMinute()*360/60-90;
 	},
 	hourDegrees(){
-		return this.currentHour()*360/12;
+		return this.currentHour()*360/12-90;
 	},
 	
 };
 
-setInterval(console.log(clock.currentSecond()),1000);
+window.addEventListener('load',function(){
+	handSeconds.style.transform = `rotate(${clock.secondDegrees()}deg)`;
+	handMinutes.style.transform = `rotate(${clock.minuteDegrees()}deg)`;
+	handHours.style.transform = `rotate(${clock.hourDegrees()}deg)`;
+});
+
+setInterval(function(){
+	handSeconds.style.transform = `rotate(${clock.secondDegrees()}deg)`;
+
+},1000);
+
+setInterval(function(){
+	handMinutes.style.transform = `rotate(${clock.minuteDegrees()}deg)`;
+	console.log(`I've change the minuteHand to ${clock.currentMinute()}`)
+},60000);
+
+setInterval(function(){
+	handHours.style.transform = `rotate(${clock.hourDegrees()}deg)`;
+	console.log(`I've change the hourHand to ${clock.currentHour()}`)
+},3600000);
